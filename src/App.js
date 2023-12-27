fd91d0dac12a47db27f4ebe1781d2f295cc18a1b
Abhijit Garai <agabhijitgarai2001@gmail.com>
2023-12-27 11:53:17 +0530

    Added a new function to edit saved items

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const App = () => {
 
   const [items, setItems] = useState("");
   const [saveItems, setSavedItems] = useState([]);
+  const [editItem, setEditItem] = useState("");
+  const [toggleButton, setToggleButton] = useState(false);
 
   const changesmade = (event) => {
     setItems(event.target.value);
@@ -18,13 +20,39 @@ const App = () => {
     setItems('');
   };
 
+  const editItems=(id)=>{
+    const todo_edit=saveItems.find((curEle,index)=>{
+        return id===index;
+      })
+    
+    setItems(todo_edit);
+    setEditItem(id);
+    setToggleButton(true);
+
+  }
+
+  const confirmEdit=()=>{
+    
+    setSavedItems(()=>{return saveItems.map((curEle,index)=>{
+        if(index===editItem) return items;
+        else return curEle;
+        
+      })});
+    setItems("");
+    setEditItem();
+    setToggleButton(false);
+    }
+
+  const deleteAll=()=>{
+    setSavedItems([]);
+  }
+
   const deleteitems = (id) => {
     setSavedItems((preVal) => {
       return preVal.filter((val, index) => {
         return index !== id;
       });
     });
-    // console.log(id + "deleted");
   }
 
 
@@ -40,11 +68,20 @@ const App = () => {
           <h1 className="text-success text-center">  ToDoList</h1>
           <br />
           <input class="form-control form-control-md" type="text"  placeholder="Enter Items" onChange={changesmade} value={items}></input>
-          <button type="button" className="btn btn-warning btn-sm my-2 mx-auto w-100" onClick={submit}>Add Items</button>
+          
+          {toggleButton ?((<button type="button" className="btn btn-warning btn-sm my-2 mx-auto w-100" onClick={confirmEdit}>Confirm Edit</button>)): (
+            <button type="button" className="btn btn-warning btn-sm my-2 mx-auto w-100" onClick={submit}>Add Items</button>
+          )
+          
+          
+          
+          }
+          
+          <button type="button" className="btn btn-info text-dark-emphasis btn-sm  mx-auto w-100" onClick={deleteAll}>Clear All Items</button>
           <hr />
           {saveItems.map((itemVal, index) => {
             /* return <li>{itemVal} <button onClick={()=>{deleteitems(index)}}>delete</button></li>; */ //without using props
-            return <ToDoList itemname={itemVal} key={index} id={index} onSelect={deleteitems} />   //with using props
+            return <ToDoList itemname={itemVal} key={index} id={index} onSelect={deleteitems} onEdit={editItems} />   //with using props
           })}
         </div>
       </div>
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -6,9 +6,16 @@ const ToDoList = (props) => {
             <ul className="list-group list-group-flush">
                 <li className="list-group-item my-1 rounded bg-body-tertiary text-capitalize">
                     {props.itemname}
-                    <button type="button" className="bg-body-tertiary mx-2 display close border-0" aria-label="Close" onClick={() => {
+                    <button type="button" className="bg-body-tertiary display close border-0" aria-label="Close" onClick={()=>{props.onEdit(props.id)}}>
+                    <span className="mx-1" >✏️</span>
+                    
+                    </button>
+                    <button type="button" className="bg-body-tertiary  display close border-0" aria-label="Close" onClick={() => {
                         props.onSelect(props.id);
-                    }}><span aria-hidden="true">&times;</span></button>
+                    }}>
+                    <span aria-hidden="true">&times;</span>
+                    
+                    </button>
                     
                 </li>
                 
